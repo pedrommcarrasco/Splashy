abb29feb2aa3996f28af60e9882838e5cfb95703
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-02-22 22:26:43 +0000

    [WIP] Home design + animation

--- Splashy/Contexts/Home/ViewControllers/HomeViewController.swift
+++ Splashy/Contexts/Home/ViewControllers/HomeViewController.swift
@@ -9,32 +9,59 @@
 import UIKit
 
 protocol HomeViewNavigationDelegate: class {
-   func homeViewControllerDidPressPlay(_ homeViewController: HomeViewController)
+    func homeViewControllerDidPressPlay(_ homeViewController: HomeViewController)
 }
 
 class HomeViewController: UIViewController {
 
-   // MARK: - OUTLETS
-   @IBOutlet weak var playButton: UIButton!
+    // MARK: - OUTLETS
+    @IBOutlet var actionButtons: [UIButton]!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var buttonsStackView: UIStackView!
 
-   // MARK: - PROPERTIES
-   var viewModel: HomeViewModel!
-   weak var navigationDelegate: HomeViewNavigationDelegate?
 
-   // MARK: - LIFECYCLE
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      setupButtons()
-   }
 
-   // MARK: - SETUP
-   private func setupButtons() {
-      playButton.roundedCorners()
-      playButton.setTitle(viewModel.playButtonText, for: .normal)
-   }
+    // MARK: - PROPERTIES
+    var viewModel: HomeViewModel!
+    weak var navigationDelegate: HomeViewNavigationDelegate?
 
-   // MARK: - ACTIONS
-   @IBAction func playButtonAction(_ sender: UIButton) {
-      self.navigationDelegate?.homeViewControllerDidPressPlay(self)
-   }
+    // MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButtons()
+        setupBackground()
+    }
+
+    // MARK: - SETUP
+    private func setupButtons() {
+        let offset = buttonsStackView.bounds.size.height
+        buttonsStackView.bounds.origin.y -= offset
+        UIView.animate(withDuration: 0.66) { [weak self] in
+            self?.buttonsStackView.bounds.origin.y += offset
+        }
+
+        actionButtons.forEach { button in
+            button.roundedCorners()
+            guard let index = actionButtons.index(of: button)?.hashValue else { return }
+            button.isHidden = true
+            UIView.animate(withDuration: 0.33, delay: 0.25*Double(index),
+                           options: .curveEaseOut, animations: {
+                button.isHidden = false
+                button.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+
+    private func setupBackground() {
+        backgroundImageView.alpha = 0
+        UIView.animate(withDuration: 1) { [weak self] in
+            self?.backgroundImageView.alpha = 1
+        }
+    }
+
+    // MARK: - ACTIONS
+    @IBAction func playButtonAction(_ sender: UIButton) {
+        self.navigationDelegate?.homeViewControllerDidPressPlay(self)
+    }
 }
