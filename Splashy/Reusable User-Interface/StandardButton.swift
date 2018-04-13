0348d138b2fd62438a34326e9b0f9d4f784a1317
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-04-13 09:03:49 +0100

    Removed isActive from each constraint and moved to NSLayoutConstraint.activate

--- Splashy/Reusable User-Interface/StandardButton.swift
+++ Splashy/Reusable User-Interface/StandardButton.swift
@@ -9,40 +9,43 @@
 import UIKit
 
 class StandardButton: UIButton {
-
+    
     // MARK: - PROPERTIES
     var image: UIImage? {
         didSet {
             self.setImage(image, for: .normal)
         }
     }
-
+    
     // MARK: - INIT
     override init(frame: CGRect) {
         super.init(frame: frame)
         initContent()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initContent()
     }
-
+    
     private func initContent() {
         asCircle()
         addVisualEffectView()
     }
-
+    
     private func addVisualEffectView() {
         guard let imageView = imageView else { return }
         let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
         visualEffectView.isUserInteractionEnabled = false
-        insertSubview(visualEffectView, belowSubview: imageView)
-
         visualEffectView.translatesAutoresizingMaskIntoConstraints = false
-        visualEffectView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        visualEffectView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        visualEffectView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        visualEffectView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        insertSubview(visualEffectView, belowSubview: imageView)
+        
+        NSLayoutConstraint.activate(
+            [visualEffectView.topAnchor.constraint(equalTo: self.topAnchor),
+             visualEffectView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+             visualEffectView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+             visualEffectView.trailingAnchor.constraint(equalTo: self.trailingAnchor)]
+        )
+        
     }
 }
